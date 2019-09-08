e7a6c2d9f000a0c85bd76df7da904dd12a780ed5
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2019-09-08 21:26:28 +0200

    test of update py

---
+++ update.py
@@ -0,0 +1,30 @@
+import os
+import re
+import fileinput
+
+VERSION_REGEX = "[0-9]+.[0-9]+.[0-9]+"
+ANSIBLE_IMAGE_VERSION = "ANSIBLE_IMAGE_VERSION"
+VERSIONS_SH = "versions.sh"
+
+
+def get_latest_ansible_version():
+    ansible_version_env = os.environ['ANSIBLE_VERSION']
+    return re.search(VERSION_REGEX, ansible_version_env).group()
+
+
+def replace_outdated_versions(new_version):
+    for line in fileinput.input(VERSIONS_SH, inplace=True):
+        if(re.search(ANSIBLE_IMAGE_VERSION, line) != None):
+            current_version = re.search(VERSION_REGEX, line).group()
+            if (current_version != new_version):
+                print("ANSIBLE_IMAGE_VERSION={}".format(new_version), end='')
+            else:
+                print(line, end='')
+        else:
+            print(line, end='')
+
+
+if __name__ == "__main__":
+    version_number = get_latest_ansible_version()
+    replace_outdated_versions(version_number)
+    print(version_number)
