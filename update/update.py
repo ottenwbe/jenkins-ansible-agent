59ff22d20114f26333d8fc9df9e739fd12fc77e5
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2019-12-23 23:09:09 +0100

    initial unit tests with mocks for git

---
+++ update/update.py
@@ -0,0 +1,81 @@
+# MIT License
+#
+# Copyright (c) 2019 Beate Ottenwälder
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import re
+import fileinput
+
+VERSION_REGEX = "[0-9]+.[0-9]+.[0-9]+"
+ANSIBLE_IMAGE_VERSION = "ANSIBLE_IMAGE_VERSION"
+VERSIONS_SH = "versions.sh"
+
+
+def _update_versons_in_git(new_version):
+    os.system("git config --global user.email 'ottenwbe.public@gmail.com'")
+    os.system("git config --global user.name 'Beate Ottenwaelder'")
+    os.system("git commit -m'Ansible Version Updated to v{}' -a".format(new_version))
+    os.system('git push --quiet origin HEAD:production')
+    os.system("git tag {}".format(new_version))
+    os.system('git push --tags')
+
+
+def _get_latest_ansible_version():
+    ansible_version_env = os.popen('yolk -V ansible').read()
+    return re.search(VERSION_REGEX, ansible_version_env).group()
+
+
+def _ansible_image_version_exists(line):
+    return re.search(ANSIBLE_IMAGE_VERSION, line)
+
+
+def _replace_line_if_image_version(new_version, line, updated):
+    current_version = re.search(VERSION_REGEX, line).group()
+    if (current_version != new_version):
+        print("{}={}".format(ANSIBLE_IMAGE_VERSION, new_version))
+        updated = True
+    else:
+        print(line)
+    return updated
+
+
+def _replace_outdated_versions(new_version):
+    updated = False
+    for line in fileinput.input(VERSIONS_SH, inplace=True):
+        if(_ansible_image_version_exists(line)):
+            updated = _replace_line_if_image_version(new_version, line, updated)
+        else:
+            print(line, end='')
+    return updated
+
+
+def main():
+    version_number = _get_latest_ansible_version()
+    updated = _replace_outdated_versions(version_number)
+    if updated:
+        print("New Version: {}".format(version_number))
+        _update_versons_in_git(version_number)
+    else:
+        print("No Update")
+
+
+if __name__ == "__main__":
+    main()
---
+++ update/update_test.py
@@ -0,0 +1,63 @@
+# MIT License
+#
+# Copyright (c) 2019 Beate Ottenwälder
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import unittest
+from update import update
+import os
+import unittest.mock
+from unittest.mock import MagicMock
+
+class UpdateTest(unittest.TestCase):
+    def setUp(self):
+        update._get_latest_ansible_version = MagicMock()   
+        update._update_versons_in_git = MagicMock()     
+        update.VERSIONS_SH = "valid.sh"
+        f = open(update.VERSIONS_SH, "w+")
+        f.write("#!/bin/bash\n\nJENKINS_IMAGE_TAG=latest\nANSIBLE_IMAGE_VERSION=0.0.0\n")
+        f.close() 
+
+    #def test_regex(self):
+    #    updated = update._replace_line_if_image_version(
+    #        "0.0.0", "ANSIBLE_IMAGE_VERSION=1.2.3", False)
+    #    self.assertTrue(updated)
+
+    #def test_failing_regex(self):
+    #   updated = update._replace_line_if_image_version(
+    #        "1.2.3", "ANSIBLE_IMAGE_VERSION=1.2.3", False)
+    #    self.assertFalse(updated)
+
+    def test_keep_version(self):
+        update._get_latest_ansible_version.return_value = "0.0.0"
+        update.main()
+        update._update_versons_in_git.assert_not_called()
+
+    def test_update_version(self):
+        update._get_latest_ansible_version.return_value = "1.0.0"
+        update.main()
+        update._update_versons_in_git.assert_called_once()
+
+    def tearDown(self):
+        os.remove(update.VERSIONS_SH)
+
+
+if __name__ == '__main__':
+    unittest.main()
