2482c280e2e84da28e3709e19d46d862497727cd
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2019-12-28 23:19:39 +0100

    test calling scripts

--- versions.sh
+++ versions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export DOCKERFILE_PATH=Dockerfile
+export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
 export IMAGE_NAME=ottenwbe/jenkins-ansible-agent
 export JENKINS_IMAGE_TAG=latest
 export ANSIBLE_IMAGE_VERSION=2.9.2
+export IMAGE_NAME=ottenwbe/jenkins-ansible-agent:${ANSIBLE_IMAGE_VERSION}
 
 
 
