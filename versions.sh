a3555c219cb64d8c4daf0567829878d8bcd5ee57
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-01-01 15:09:44 +0100

    initial update script

--- versions.sh
+++ versions.sh
@@ -2,4 +2,4 @@
 export DOCKERFILE_PATH=Dockerfile
 export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
 export JENKINS_IMAGE_TAG=latest
-export ANSIBLE_IMAGE_VERSION=2.9.2
+export ANSIBLE_IMAGE_VERSION=2.9.1
