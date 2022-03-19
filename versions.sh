b1dc25426c3f347dce4502196c107dc62541af4b
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2022-03-19 19:15:10 +0100

    jdk11

--- versions.sh
+++ versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DOCKERFILE_PATH=Dockerfile
 export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
-export JENKINS_IMAGE_TAG=latest
+export JENKINS_IMAGE_TAG=jdk11
 export ANSIBLE_IMAGE_VERSION=2.9.2
 
 
