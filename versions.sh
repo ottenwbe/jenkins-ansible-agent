7e1553302d2476de60352027227573276de7ad23
Beate Ottenwälder <ottenwbe@users.noreply.github.com>
2022-03-19 20:38:20 +0100

    Master -> Production (#16)
    * updated Dockerfile

--- versions.sh
+++ versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DOCKERFILE_PATH=Dockerfile
 export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
-export JENKINS_IMAGE_TAG=latest
-export ANSIBLE_IMAGE_VERSION=2.10.1
+export JENKINS_IMAGE_TAG=jdk11
+export ANSIBLE_IMAGE_VERSION=2.10.0
