9582dfb737878b4ca9e93eafabb2610bd4767453
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2022-03-19 19:21:52 +0100

    Ansible Version Updated to v5.5.0

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 export DOCKERFILE_PATH=Dockerfile
 export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
 export JENKINS_IMAGE_TAG=jdk11
-export ANSIBLE_IMAGE_VERSION=2.9.2
+export ANSIBLE_IMAGE_VERSION=5.5.0
 
 
 
