a971037d2f10885d7a2b7861010be96f89738e3c
Beate Ottenwälder <ottenwbe@users.noreply.github.com>
2022-03-19 19:39:44 +0100

    [Fix] Base Image Upgrade (#12)
    * new base image (jenkins/jdk11)

--- versions.sh
+++ versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DOCKERFILE_PATH=Dockerfile
 export DOCKER_REPO=ottenwbe/jenkins-ansible-agent
-export JENKINS_IMAGE_TAG=latest
-export ANSIBLE_IMAGE_VERSION=2.9.2
+export JENKINS_IMAGE_TAG=jdk11
+export ANSIBLE_IMAGE_VERSION=5.5.0
 
 
 
